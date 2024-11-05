2a0b3be6475d65bb3eddf462a8bc759634a31ea8
Shivam Kumar Pathak <shivamkumarpathakmisu@gmail.com>
2024-11-05 18:19:19 +0530

    Frontend URL added

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://bookstore-frontend-nd98.onrender.com/",
     methods:["POST","GET","PUT","DELETE"],
     credentials:true
 }))
